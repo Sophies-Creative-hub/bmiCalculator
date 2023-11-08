0ba8bde55800d367dd8065a01258031650214c01
Sophie Ischenko <82713289+Sophies-Creative-hub@users.noreply.github.com>
2023-11-08 11:24:59 +0100

    Update bmi.test.js

--- bmi.test.js
+++ bmi.test.js
@@ -1,7 +1,7 @@
 import { calculateBMI } from './frontend/main';
 
 test('Berechnung des BMI', () => {
-  expect(calculateBMI(70, 175)).toBe(24.49);
-  expect(calculateBMI(85, 190)).toBe(23.55);
-  expect(calculateBMI(65, 160)).toBe(25.39);
+  expect(calculateBMI(70, 175)).toBe(23);
+  expect(calculateBMI(85, 190)).toBe(24);
+  expect(calculateBMI(65, 160)).toBe(25);
 });
